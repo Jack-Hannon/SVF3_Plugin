8317a32f96e2611ddf337019329a88d22ed01ea4
Jan Wilczek <24981875+JanWilczek@users.noreply.github.com>
2026-01-23 09:29:22 +0100

    Added comments

--- audio_plugin/audio_plugin.cpp
+++ audio_plugin/audio_plugin.cpp
@@ -2,3 +2,5 @@
 
 #include "source/PluginProcessor.cpp"
 #include "source/PluginEditor.cpp"
+
+// #include all additional .cpp files below
--- audio_plugin/audio_plugin.h
+++ audio_plugin/audio_plugin.h
@@ -23,3 +23,5 @@ END_JUCE_MODULE_DECLARATION
 #include <juce_audio_processors/juce_audio_processors.h>
 #include "include/PluginProcessor.h"
 #include "include/PluginEditor.h"
+
+// #include all additional header files below
